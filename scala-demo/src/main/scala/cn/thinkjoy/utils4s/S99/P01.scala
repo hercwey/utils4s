3b16bda0c300fd00f92439ad0353fc35eabc57d5
paul.wey <gp.wei@hnair.com>
2019-08-21 16:21:52 +0800

    change

--- scala-demo/src/main/scala/cn/thinkjoy/utils4s/S99/P01.scala
+++ scala-demo/src/main/scala/cn/thinkjoy/utils4s/S99/P01.scala
@@ -7,6 +7,6 @@ object P01 {
   def last[A](ls: List[A]): A = ls.last
 
   def main(args: Array[String]) {
-    println(last(List(1, 1, 2, 3, 5, 8)))
+    println(last(List(1, 1, 2, 3, 5, 8, "z")))
   }
 }
